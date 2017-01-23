07115b38422746987b2fd9692e2629f3d618f9e0
Thomas Hodges <thodges@javascriptonline.org>
2017-01-22 22:25:27 -0800

    add and remove items from array with binding

--- app/components/user.component.ts
+++ app/components/user.component.ts
@@ -3,21 +3,35 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'user',
   template: `
-  <h1>Hello, {{name}}</h1>
+  <h1>{{name}}</h1>
   <p><strong>Email:</strong> {{email}}</p>
   <p><strong>Address:</strong> {{address.street}} {{address.city}} {{address.state}}</p>
   <button (click) = "toggleHobbies()">{{showHobbies? "hide hobbies" : "show hobbies"}}</button>
   <div *ngIf = "showHobbies">
     <h3>Hobbies</h3>
     <ul>
-      <li *ngFor = "let hobby of hobbies">
-        {{hobby}}
+      <li *ngFor = "let hobby of hobbies; let i = index">
+        {{hobby}} <button (click)="deleteHobby(i)">X</button>
       </li>
     </ul>
+    <form (submit)="addHobby(hobby.value)">
+      <label>add hobby:</label><br />
+      <input type="text" #hobby /><br />
+    </form>
   </div>
+  <hr />
+  <h3>edit user:</h3>
   <form>
     <label>name: </label><br />
-    <input type="text"  name="name" [(ngModel)]="name" /> 
+    <input type="text"  name="name" [(ngModel)]="name" /> <br/>
+    <label>eMail: </label><br />
+    <input type="text"  name="email" [(ngModel)]="email" /> <br/>
+    <label>street address: </label><br />
+    <input type="text"  name="address.street" [(ngModel)]="address.street" /> <br/>
+    <label>city: </label><br />
+    <input type="text"  name="address.city" [(ngModel)]="address.city" /> <br/>
+    <label>state: </label><br />
+    <input type="text"  name="address.state" [(ngModel)]="address.state" /> <br/>
   </form>
   `,
 })
@@ -42,6 +56,14 @@ export class UserComponent {
 
   toggleHobbies(){
     this.showHobbies = !this.showHobbies;
+  }
+
+  addHobby(hobby: string) {
+    this.hobbies.push(hobby);
+  }
+
+  deleteHobby(index: int){
+    this.hobbies.splice(index, 1);
   }
 }
 
